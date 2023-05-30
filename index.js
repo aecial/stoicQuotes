d14f6e722e49d36437dbf4e1e0fb923d9711515d
Teddy Pascual <teddylayson2000@gmail.com>
2023-05-30 09:06:58 +0800

    integrated bootstrap and improved design

--- index.js
+++ index.js
@@ -4,22 +4,22 @@ window.onload = window.navigator.vibrate(600);
 window.onload = newQuote;
 
 function newQuote() {
-    // fetch ('the url')
-    fetch('https://stoic-quotes.com/api/quote')
-        // then the result from that api which is res will be converted to json
-        .then(res => {
-            return res.json();
-        })
-        // then that data that is converted to json will be passed on as value of quote and author
-        .then(data => {
-            quoteContainer.innerHTML = `"` + data.text + `"`;
-            authorContainer.innerHTML = `- ` + data.author;
-        })
-        // if there is an error print it out on the screen
-        .catch(error => console.log(error));
+  // fetch ('the url')
+  fetch("https://stoic-quotes.com/api/quote")
+    // then the result from that api which is res will be converted to json
+    .then((res) => {
+      return res.json();
+    })
+    // then that data that is converted to json will be passed on as value of quote and author
+    .then((data) => {
+      quoteContainer.innerHTML = `"` + data.text + `"`;
+      authorContainer.innerHTML = data.author;
+    })
+    // if there is an error print it out on the screen
+    .catch((error) => console.log(error));
 }
 function tweet() {
-    // create a new tweet in another tab
-    const newTweet = `https://twitter.com/intent/tweet?text=${quoteContainer.textContent} ${authorContainer.textContent}`;
-    window.open(newTweet, "_blank");
+  // create a new tweet in another tab
+  const newTweet = `https://twitter.com/intent/tweet?text=${quoteContainer.textContent} ${authorContainer.textContent}`;
+  window.open(newTweet, "_blank");
 }
